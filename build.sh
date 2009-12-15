671e952886a9e1f4fc067d87f31af3d4dbf7c917
Sam Leffler <sleffler@google.com>
2009-12-15 14:18:34 -0800

    fixup paths for kernel modules

--- build.sh
+++ build.sh
@@ -6,7 +6,6 @@
 
 set -e
 
-BUILDDIR=$PWD/build/lib/modules/2.6.31.4-chromeos-intel-menlow/drivers/net/wireless
 export KERNELDIR=$PWD/../../build/kernels/kernel-i386-intel-menlow/linux-2.6.31.4/
 export CROSS_COMPILE=
 export ARCH=i386
@@ -15,7 +14,8 @@ export ARCH=i386
 # and dpkg-buildpackage
 NUM_JOBS=`cat /proc/cpuinfo | grep processor | awk '{a++} END {print a}'`
 
-rm -rf $PWD/build
+BUILDDIR=$PWD/build
+rm -rf $BUILDDIR
 mkdir -p $BUILDDIR
 
 pushd wlan_src
